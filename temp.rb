3e5d0effc707d4814c59eb1d6ee7cd3886a3dcae
Kristaps Grinbergs <fassko@gmail.com>
2012-02-06 16:18:03 +0200

    Edited to show grades symbol.

--- temp.rb
+++ temp.rb
@@ -2,4 +2,4 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-puts Nokogiri::HTML(open('http://www.rs.lv/index.php?aid=3&id=1&action=body&print=1')).search("//font[@style='font-weight: bold;']/b").inner_html.to_s.gsub("\260"," ")
+puts Nokogiri::HTML(open('http://www.rs.lv/index.php?aid=3&id=1&action=body&print=1')).search("//font[@style='font-weight: bold;']/b").inner_html.to_s.gsub("\260","°")
