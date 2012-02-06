4274842288229f635ebc4c46f71c1548f1e0143e
Kristaps Grinbergs <fassko@gmail.com>
2012-02-06 23:27:15 +0200

    Papildinājumi atrādāmajai informācijai.

--- temp.rb
+++ temp.rb
@@ -2,4 +2,14 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-puts Nokogiri::HTML(open('http://www.rs.lv/index.php?aid=3&id=1&action=body&print=1')).search("//font[@style='font-weight: bold;']/b").inner_html.to_s.gsub("\260","°")
+doc = Nokogiri::HTML(open('http://www.rs.lv/index.php?aid=3&id=1&action=body&print=1'))
+
+puts "Pašlaik Rīgā: " + doc.search("//font[@style='font-weight: bold;']/b").inner_html.to_s.gsub("\260","°")
+puts "\n"
+
+doc.search("//td[@class='meteo_graph']/table/tr").each do |tr|
+  if (tr.children.first.inner_html != "Datums/stunda")
+    date_parts = tr.children.first.inner_html.split("/")
+    puts "#{date_parts[0]}.#{Time.now.strftime("%M.%Y")} #{date_parts[1]}:00  #{tr.children.last.inner_html}°C"
+  end
+end
