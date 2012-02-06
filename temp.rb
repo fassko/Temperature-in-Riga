f1a10d71c27700ebea2bed8e9f9ca73532d2f23b
Kristaps Grinbergs <fassko@gmail.com>
2012-02-06 10:28:11 +0200

    First commit

---
+++ temp.rb
@@ -0,0 +1,5 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+puts Nokogiri::HTML(open('http://www.rs.lv/index.php?aid=3&id=1&action=body&print=1')).search("//font[@style='font-weight: bold;']/b").inner_html.to_s.gsub("\260"," ")
