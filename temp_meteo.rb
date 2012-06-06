d82df468440fbdbd5b6d8cfe8258143a916a32ce
Kristaps Grinbergs <fassko@gmail.com>
2012-06-06 09:48:12 +0300

    Some updates.

--- temp_meteo.rb
+++ temp_meteo.rb
@@ -4,15 +4,19 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open('http://www.meteo.lv/public/'))
 
-puts "#{doc.search("#laikazinas/h2").inner_html.gsub("  "," ")}"
+if ARGV[0]
+  puts "#{doc.search("#laikazinas/h2").inner_html.gsub("  "," ")}"
 
-i = 0
-doc.search("#laikazinas/table/tr").each do |tr|
-  if i == 2
-    puts "#{tr.children.first.inner_html} #{tr.children[2].children[0].attributes['title'].value}"
-  else
-    puts "#{tr.children.first.inner_html} #{tr.children[2].inner_html}\n"
+  i = 0
+  doc.search("#laikazinas/table/tr").each do |tr|
+    if (tr.children.first.inner_html == "Vēja virziens:")
+      puts "#{tr.children.first.inner_html} #{tr.children[2].children[0].attributes['title'].value}"
+    else
+      puts "#{tr.children.first.inner_html} #{tr.children[2].inner_html}\n"
+    end
+
+    i = i + 1
   end
-
-  i = i + 1
+else
+  puts "#{doc.search("#laikazinas/h2").inner_html.split(":  ")[1]} #{doc.search("#laikazinas/table/tr/td").first.inner_html}"
 end
